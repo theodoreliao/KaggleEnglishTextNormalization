b195c5ae4bfa4ce4a4b3650fe624949827a4dbd9
Junia George <junegal11@gmail.com>
2017-09-22 21:24:16 -0700

    Added roman numeral function
    Found it from stack overflow. We should check functionality

--- src/TestDataNormalizer.java
+++ src/TestDataNormalizer.java
@@ -54,7 +54,69 @@ public class TestDataNormalizer {
     }
 
     private static String readRomanNumeral(String before) {
-        return "";
+        int decimal = 0;
+
+        String romanNumeral = before.toUpperCase();
+        for(int x = 0;x<romanNumeral.length();x++)
+        {
+            char convertToDecimal = before.charAt(x);
+
+            switch (convertToDecimal)
+            {
+                case 'M':
+                    decimal += 1000;
+                    break;
+
+                case 'D':
+                    decimal += 500;
+                    break;
+
+                case 'C':
+                    decimal += 100;
+                    break;
+
+                case 'L':
+                    decimal += 50;
+                    break;
+
+                case 'X':
+                    decimal += 10;
+                    break;
+
+                case 'V':
+                    decimal += 5;
+                    break;
+
+                case 'I':
+                    decimal += 1;
+                    break;
+            }
+        }
+        if (romanNumeral.contains("IV"))
+        {
+            decimal-=2;
+        }
+        if (romanNumeral.contains("IX"))
+        {
+            decimal-=2;
+        }
+        if (romanNumeral.contains("XL"))
+        {
+            decimal-=10;
+        }
+        if (romanNumeral.contains("XC"))
+        {
+            decimal-=10;
+        }
+        if (romanNumeral.contains("CD"))
+        {
+            decimal-=100;
+        }
+        if (romanNumeral.contains("CM"))
+        {
+            decimal-=100;
+        }
+        return readNumber(Integer.toString(decimal));
     }
 
     public static String spreadAcronym(String input){
@@ -67,6 +129,8 @@ public class TestDataNormalizer {
     }
 
     public static String readNumber(String input){
+        int number = Integer.parseInt(input);
+
         return "";
     }
 
